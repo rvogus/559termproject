94205dc9f182d909a4667dd8e8695bcf59b977f1
Rachel Vogus <rvogus@gmail.com>
2020-04-24 13:38:32 -0400

    second try

---
+++ src/build/precache-manifest.426c6f72fd2d6efe7765cb7d5b159be8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da020f7e58a7da8ebe2841ec51b8d528",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a70a4fa17e74103d9006",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "03d6dfdb49e1f0428b3f",
+    "url": "/static/js/2.1fc4d83f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1fc4d83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a70a4fa17e74103d9006",
+    "url": "/static/js/main.4c0aa22f.chunk.js"
+  },
+  {
+    "revision": "e988c064c70788dbc50e",
+    "url": "/static/js/runtime-main.a56884a5.js"
+  }
+]);
